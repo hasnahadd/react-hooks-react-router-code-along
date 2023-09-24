8da6fc645989d802d112b13164e7302306269a43
hasna <manelhasnahaddoud@gmail.com>
2023-09-24 22:13:21 +0100

    done

--- src/index.js
+++ src/index.js
@@ -1,21 +1,66 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+/* Add NavLink to import */
+import { BrowserRouter, Route, NavLink, Switch } from "react-router-dom";
 
-function Home() {
+/* Add basic styling for NavLinks */
+const linkStyles = {
+  display: "inline-block",
+  width: "50px",
+  padding: "12px",
+  margin: "0 6px 6px",
+  background: "blue",
+  textDecoration: "none",
+  color: "white",
+};
+
+/* define the NavBar component */
+function NavBar() {
   return (
     <div>
-      <h1>Home!</h1>
+      <NavLink
+        to="/"
+        /* set exact so it knows to only set activeStyle when route is deeply equal to link */
+        exact
+        /* add styling to Navlink */
+        style={linkStyles}
+        /* add prop for activeStyle */
+        activeStyle={{
+          background: "darkblue",
+        }}
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to="/about"
+        exact
+        style={linkStyles}
+        activeStyle={{
+          background: "darkblue",
+        }}
+      >
+        About
+      </NavLink>
+      <NavLink
+        to="/login"
+        exact
+        style={linkStyles}
+        activeStyle={{
+          background: "darkblue",
+        }}
+      >
+        Login
+      </NavLink>
     </div>
   );
 }
 
+function Home() {
+  return <h1>Home!</h1>;
+}
+
 function About() {
-  return (
-    <div>
-      <h1>This is my about component!</h1>
-    </div>
-  );
+  return <h1>This is my about component!</h1>;
 }
 
 function Login() {
@@ -35,17 +80,18 @@ function Login() {
   );
 }
 
-// Step 2. Change so router is coordinating what is displayed
+/* add the NavBar component to our render method */
 ReactDOM.render(
   <BrowserRouter>
+    <NavBar />
     <Switch>
-      <Route path="/about">
+      <Route exact path="/about">
         <About />
       </Route>
-      <Route path="/login">
+      <Route exact path="/login">
         <Login />
       </Route>
-      <Route path="/">
+      <Route exact path="/">
         <Home />
       </Route>
     </Switch>
